ed2a154f02e2da030660d9e4653d676dde881b31
markkevinbrin <58113589+markkevinbrin@users.noreply.github.com>
2019-11-30 23:38:06 +0800

    update - is238 config

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'Elekid123!@-');
 define('DB_NAME', 'is238');
  
 /* Attempt to connect to MySQL database */
