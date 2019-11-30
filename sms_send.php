c50fab1d80016bacd57b674ee485bf7934b9f25f
topepers <49813969+topepers@users.noreply.github.com>
2019-12-01 02:59:37 +0800

    Update sms_send.php

--- sms_send.php
+++ sms_send.php
@@ -4,7 +4,7 @@ require_once(__DIR__ . '/include.php');
 use Globe\Connect\Sms;
 
 //$sms = new Sms('0123', 'MQWvyMejeJG9w-rsAUPlzs7XvGIKLnarjFpLr5NCY5k');
-$sms = new Sms('0567', 'vRAPHZPExy7EUSbmzSJL-y4PJwRYfjHSC9A8uX0pHgc');
+$sms = new Sms('0567', 'WqgqkvVu93th6x4UTThwrQJWOCalmrDihXSQBnvh2WQ');
 
 
 // send message
@@ -13,7 +13,7 @@ $sms = new Sms('0567', 'vRAPHZPExy7EUSbmzSJL-y4PJwRYfjHSC9A8uX0pHgc');
 //$sms->setClientCorrelator('12345');
 //echo $sms->sendMessage();
 
-$sms->setReceiverAddress('9273289628');
+$sms->setReceiverAddress('9173186699');
 $sms->setMessage('ako ang iyong konsyensya awoooo');
 $sms->setClientCorrelator('12345');
 echo $sms->sendMessage();
